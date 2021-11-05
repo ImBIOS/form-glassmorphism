f7c5705f2d8c9ffd8355725b6442cfe994575a84
Fawwaz-abdurrahim <fwwz.id@gmail.com>
2021-11-05 08:54:41 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Tilt from "react-parallax-tilt";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App h-screen w-screen relative overflow-hidden bg-gradient-to-r from-purple-900 to-pink-600 flex justify-center items-center">
+			<Tilt
+				tiltReverse={true}
+				transitionSpeed={500}
+				glareEnable={true}
+				glareReverse={true}
+				glareMaxOpacity="0.15"
+			>
+				<div className="card-transparent">
+					<p className="card-title">Log In</p>
+					<div className="w-max m-auto flex flex-col">
+						<input
+							type="text"
+							className="bg-transparent border-opacity-0 focus:outline-none text-white mt-5"
+							placeholder="username"
+						/>
+						<input
+							type="password"
+							className="bg-transparent border-opacity-0 focus:outline-none text-white mt-5"
+							placeholder="password"
+						/>
+					</div>
+					<div className="w-max mx-auto md:mt-8 mt-5 pb-10">
+						<button className="button">Login</button>
+					</div>
+				</div>
+			</Tilt>
+		</div>
+	);
 }
 
 export default App;
